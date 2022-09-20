8cd19a28464ac1fce923fcd4d926ed619dc61952
piero091100 <piero9965@gmail.com>
2022-09-20 15:47:39 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Potato miPotato = new Potato();
-        miPotato.QuitarBrazo();
-        miPotato.QuitarBrazo();
-        miPotato.QuitarBrazo();
-        System.out.println(miPotato.brazos);
+        int resultado;
+        resultado = suma(20, 20, 20);
+
+        System.out.println(resultado);
+
+        Coche miCoche = new Coche();
+        miCoche.AumentarPuertas();
+        miCoche.AumentarPuertas();
+        System.out.println(miCoche.puertas);
     }
 
     public static int suma(int a, int b, int c){
         return a + b + c;
     }
+}
 
+class Coche {
+    public int puertas = 0;
+
+    public void AumentarPuertas(){
+        this.puertas++;
+    }
 }
 
